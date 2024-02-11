87a27232eec1632ecc6b0cd8976bc05b4e6065f7
bishal189 <bishalmurmu150@gmail.com>
2024-02-11 15:39:03 +0545

    added global state variable for login user

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
-import  "./App.css"
-import Routing from './components/Routing/Routing'
-import Navbar from './components/Navbar/Navbar'
-
+import React from "react";
+import "./App.css";
+import Routing from "./components/Routing/Routing";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const jwtUser = jwtDecode(jwt);
+      if (Date.now() >= jwtUser.exp * 1000) {
+        localStorage.removeItem("token");
+        location.reload();
+      } else {
+        setUser(jwtUser);
+      }
+    } catch (err) {}
+  }, []);
+
+
+
   return (
-    <div className='App'>
-      <Navbar/>
+    <div className="App">
+      <Navbar user={user}/>
 
       <main>
-        
-      <Routing/>
+        <Routing />
       </main>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,13 +3,13 @@ import "./Navbar.css";
 import rocket from "../../assets/rocket.png";
 import star from "../../assets/glowing-star.png";
 import idButton from "../../assets/id-button.png";
-import memo from "../../assets/memo.png"
-import order from "../../assets/package.png"
-import lock from "../../assets/locked.png"
+import memo from "../../assets/memo.png";
+import order from "../../assets/package.png";
+import lock from "../../assets/locked.png";
 import LinkIcon from "./LinkIcon";
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
   return (
     <nav className="navbar align_center">
       <div className="align_center">
@@ -22,17 +22,25 @@ const Navbar = () => {
         </form>
       </div>
       <div className="align_center navbar_links">
-       <LinkIcon title="home" link="/" emoji={rocket}/>
-       <LinkIcon title="Products" link='/products' emoji={star} />
-       <LinkIcon title="Login" link="/login" emoji={idButton} />
-
-       <LinkIcon title="SignUP" link='/signup' emoji={memo}/>
-       <LinkIcon title='My Orders' link='/myorders' emoji={order} />
-       <LinkIcon title='Logout' link='/logout' emoji={lock} />
+        <LinkIcon title="home" link="/" emoji={rocket} />
+        <LinkIcon title="Products" link="/products" emoji={star} />
         
-        <NavLink to={"/cart"} className="align_center">
-            Cart <p className="align_center cart_counts">0</p>
-        </NavLink>
+        { !user && <>
+          <LinkIcon title="Login" link="/login" emoji={idButton} />
+
+        <LinkIcon title="SignUP" link="/signup" emoji={memo} />
+        </>}
+        
+        {user && (
+          <>
+            <LinkIcon title="My Orders" link="/myorders" emoji={order} />
+            <LinkIcon title="Logout" link="/logout" emoji={lock} />
+
+            <NavLink to={"/cart"} className="align_center">
+              Cart <p className="align_center cart_counts">0</p>
+            </NavLink>
+          </>
+        )}
       </div>
     </nav>
   );
