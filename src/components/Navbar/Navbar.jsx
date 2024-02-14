7dd7c3bf3fd390e7931fe03a8106f179320e1d06
bishal189 <bishalmurmu150@gmail.com>
2024-02-14 10:25:45 +0545

    search functionality added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,21 +7,44 @@ import memo from "../../assets/memo.png";
 import order from "../../assets/package.png";
 import lock from "../../assets/locked.png";
 import LinkIcon from "./LinkIcon";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "../../context/userContext";
 import cartContext from "../../context/cartContext";
+import { useState } from "react";
 
 const Navbar = () => {
-  const user=useContext(UserContext)
-  const { cart }=useContext(cartContext)
+  const user = useContext(UserContext);
+  const [search, setSearch] = useState("");
+
+  const { cart } = useContext(cartContext);
+  const navigate=useNavigate()
+
+  //handling submit
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(search.trim()!==""){
+      navigate(`/products?search=${search.trim()}`)
+    }
+   
+    
+  }
+
+  
 
   return (
     <nav className="navbar align_center">
       <div className="align_center">
         <h1 className="navbar_heading">CartWish</h1>
-        <form className="align_center  navbar_form" action="">
-          <input type="text" className="navbar_search" placeholder="Search" />
+        <form className="align_center  navbar_form" action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="navbar_search"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <button type="submit" className="search_button">
             Search
           </button>
@@ -30,13 +53,15 @@ const Navbar = () => {
       <div className="align_center navbar_links">
         <LinkIcon title="Home" link="/" emoji={rocket} />
         <LinkIcon title="Products" link="/products" emoji={star} />
-        
-        { !user && <>
-          <LinkIcon title="Login" link="/login" emoji={idButton} />
 
-        <LinkIcon title="Signup" link="/signup" emoji={memo} />
-        </>}
-        
+        {!user && (
+          <>
+            <LinkIcon title="Login" link="/login" emoji={idButton} />
+
+            <LinkIcon title="Signup" link="/signup" emoji={memo} />
+          </>
+        )}
+
         {user && (
           <>
             <LinkIcon title="My Orders" link="/myorders" emoji={order} />
